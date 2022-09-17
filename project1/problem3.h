1dbd89a7aa47d2879d11cfab1a33cb547f28c8a7
mcjonker <mcjonker9@gmail.com>
2022-09-16 22:59:37 -0400

    updated spacing on header

--- project1/problem3.h
+++ project1/problem3.h
@@ -1,11 +1,8 @@
 // Copyright 2022 Mitchell Jonker
 // This file contains the prototypes for the functions.
-
 #ifndef _PROBLEM3_H_
 #define _PROBLEM3_H_
-
 int Reverse(int value);
 int Reverse(int value, int flips);
 int MatchWithReverseDigits(int value, int compare);
-
 #endif  // _PROBLEM3_H_
