c3c6ae6df77ed5499db16ff5e6636b9322b5fe24
mcjonker <mcjonker9@gmail.com>
2022-11-17 12:42:22 -0500

    Probability

--- program5/probability.cc
+++ program5/probability.cc
@@ -1,5 +1,97 @@
 // Copyright 2022 Mitchell Jonker
 // Class implementation by Mitchell Jonker
 // implement Probability class functionality here
+#include "probability.h"
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+using std::to_string;
+namespace csce240_program5  {
+Probability::Probability(int numer, int denom) {
+  // Constructor from two integers.
+  if (numer >= 0) {
+    SetNumerator(numer);
+  } else {
+    SetNumerator(0);
+  }
+  if (denom > 0) {
+    SetDenominator(denom);
+  } else {
+    SetDenominator(1);
+  }
+  if (numer/denom > 1 || numer/denom < 0) {
+    SetNumerator(0);
+    SetDenominator(1);
+  }
+}
+Probability::Probability(Fraction z) {
+  // Constructor from Fraction
+  int numer = z.GetNumerator();
+  int denom = z.GetDenominator();
+  if (numer >= 0) {
+    SetNumerator(numer);
+  } else {
+    SetNumerator(0);
+  }
+  if (denom > 0) {
+    SetDenominator(denom);
+  } else {
+    SetDenominator(1);
+  }
+  if (numer/denom > 1 || numer/denom < 0) {
+    SetNumerator(0);
+    SetDenominator(1);
+  }
+}
+bool Probability::operator ==(Probability b) const {
+  if ((GetNumerator() * b.GetDenominator()) == (GetDenominator() * b.GetNumerator())) {
+    return true;
+  }
+  //cout << GetNumerator() << "/" << GetDenominator() << endl;
+  //cout << b.GetNumerator() << "/" << b.GetDenominator() << endl;
+  //cout << (GetNumerator() * b.GetDenominator()) << "|" << (GetDenominator() * b.GetNumerator()) << endl;
+  return false;
+}
+/*
+Probability operator *(Probability b) const {
+  int numer = numer_ * b.GetNumerator();
+  int denom = denom_ * b.GetDenominator();
+  Probability f;
+  f.SetNumerator(numer);
+  f.SetDenominator(denom);
+  return f;
+}
+*/
+void Probability::Multiply(int mult) {
+  //cout << "mult: " << mult << endl;
+  //cout << "numer: " << GetNumerator() << endl;
+  double product = mult*GetNumerator();
+  //cout << "product: " << product << endl;
+  //cout << "fract: " << product << "/" << GetDenominator() << endl;
+  double prob = product/GetDenominator();
+  //cout << "probability: " << prob << endl;
+  if (prob <= 1 && prob >= 0) {
+    //cout << "ifmet" << endl;
+    SetNumerator(product);
+  } else {
+    //cout << "ifnot" << endl;
+    SetNumerator(GetNumerator());
+  }
+  
+}
+string OddsInFavor() {
+  
+  
+  return "";
+}
+void Probability::Divide(int dividend) {
+  if (dividend > 0) {
+    double product = dividend*GetDenominator();
+    //cout << "Prod: " << product << endl;
+    SetDenominator(product);
+  }
+}
 
-
+}  // end namespace csce240_program5
--- program5/probability.h
+++ program5/probability.h
@@ -1,24 +1,46 @@
+// Copyright 2022 Mitchell Jonker
+// Header implementation by Mitchell Jonker
+#ifndef PROGRAM5_PROBABILITY_H_
+#define PROGRAM5_PROBABILITY_H_
+#include <string>
+#include "fraction.h"
+using std::string;
 namespace csce240_program5 {
-
 // Define the Probability class as a child of the Fraction class
 // Probabilities are always between 0 and 1, inclusive
 // Invalid operations should leave the object unchanged.
 class Probability : public Fraction {
-  // create a constructor should take two integers and create a probability,
-  // defaulting the numerator to 0 and the denominator to 1 if they are not
-  // sent as arguments - invalid initialization should result in the
-  // probability being 0
-
-  // create a constructor that takes a Fraction and creates a probability
-  // if the fraction is not a valid probability, the probability should
-  // be set to 0
-
-  // Redefine any of the inherited functions that need to be modified
-  // in order to work properly for a Probability.
-
-  // OddsInFavor function should return a string for the odds that correspond
-  // to the probability. For example, the probability 2/5 is equivalent to
-  // the odds in favor of 2:3
+  public:
+    explicit Probability(int numer = 0, int denom = 1);
+    explicit Probability(Fraction z);
+    // Redefine inheritied funtions if needed
+    bool operator ==(Probability b) const;
+    //Fraction operator *(Fraction b) const;
+    string OddsInFavor();
+    void Multiply(int mult);
+    void Divide(int dividend);
+    //void SetNumerator(int numer);
+    //void SetDenominator(int denom);
+  
+  private:
+    int numer_;
+    int denom_;
+  
 };
-
 }  // end namespace csce240_program5
+// create a constructor should take two integers and create a probability,
+// defaulting the numerator to 0 and the denominator to 1 if they are not
+// sent as arguments - invalid initialization should result in the
+// probability being 0
+
+// create a constructor that takes a Fraction and creates a probability
+// if the fraction is not a valid probability, the probability should
+// be set to 0
+
+// Redefine any of the inherited functions that need to be modified
+// in order to work properly for a Probability.
+
+// OddsInFavor function should return a string for the odds that correspond
+// to the probability. For example, the probability 2/5 is equivalent to
+// the odds in favor of 2:3
+#endif  // PROGRAM5_PROBABILITY_H_
